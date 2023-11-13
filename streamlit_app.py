ad3f6f9fa8f3f83811a1573c99a4771a9abb6a39
sathyapat <147024683+sathyapat@users.noreply.github.com>
2023-11-13 17:13:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ streamlit.dataframe(my_data_rows)
 # Challenge add another text input like Kiwa
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# This will not work correctly, but just go it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
