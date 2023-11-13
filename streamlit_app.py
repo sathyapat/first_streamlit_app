e41c0d7029083b51e82b0119bc835e557d102839
sathyapat <147024683+sathyapat@users.noreply.github.com>
2023-11-13 17:09:10 +0800

    Update streamlit_app.py
    add new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
+
+# Challenge add another text input like Kiwa
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
